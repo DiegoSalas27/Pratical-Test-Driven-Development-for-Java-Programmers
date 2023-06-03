ccb3f21948cc03895d46e62412a42dd03bb1cdb0
DiegoSalas27 <dominicsc2hs@gmail.com>
2023-06-02 22:07:58 -0400

    refactor: update checkISBN method

--- src/com/dominic/isbntools/ValidateISBN.java
+++ src/com/dominic/isbntools/ValidateISBN.java
@@ -12,12 +12,11 @@ public class ValidateISBN {
 		
 		if (isbn.length() == LONG_ISBN_LENGTH) {
 			return isThisAValidLongISBN(isbn, total);
-		} else {
-			if (isbn.length() != SHORT_ISBN_LENGTH)
-				throw new NumberFormatException("ISBN numbers must be 10 digits long");
-
+		} else if (isbn.length() == SHORT_ISBN_LENGTH) {
 			return isThisAValidShortISBN(isbn, total);
 		}
+		
+		throw new NumberFormatException("ISBN numbers must be 10 or 13 digits long");
 	}
 
 	private boolean isThisAValidShortISBN(String isbn, int total) {
