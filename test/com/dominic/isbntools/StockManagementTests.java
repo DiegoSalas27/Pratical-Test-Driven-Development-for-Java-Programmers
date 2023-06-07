457eb09191d8f3e475be7deb5e0e86d6f2cc6c72
DiegoSalas27 <dominicsc2hs@gmail.com>
2023-06-06 22:30:03 -0400

    chore: add stubs to test mocking external dependencies

---
+++ test/com/dominic/isbntools/StockManagementTests.java
@@ -0,0 +1,37 @@
+package com.dominic.isbntools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StockManagementTests {
+
+	@Test
+	void testCanGetACorrectLocatorCode() {
+		
+		ExternalISBNDataService testWebService = new ExternalISBNDataService() {
+			
+			@Override
+			public Book lookup(String isbn) {
+				return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
+			}
+		};
+		
+		ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
+			
+			@Override
+			public Book lookup(String isbn) {
+				return null;
+			}
+		};
+		
+		StockManager stockManager = new StockManager();
+		stockManager.setWebService(testWebService);
+		stockManager.setDatabaseService(testDatabaseService);
+		
+		String isbn = "0140177396";
+		String locatorCode = stockManager.getLocatorCode(isbn);
+		assertEquals("7396J4", locatorCode); // expectedValue, actualValue
+	}
+
+}
